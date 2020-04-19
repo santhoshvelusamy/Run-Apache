931a96fd0ca296964fd609d4dc91d1f03029d56a
Natchimuthu Velusamy <vnachimu@ford.com>
2020-04-19 20:30:59 +0530

    changed-readme

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -21,6 +21,22 @@ pipeline {
                 echo 'Deploying....'
             }
         }
+        stage('Push to Master') {
+            steps {
+                echo 'Pushig to Master....'
+                sh"""
+                set +x
+                git config --global user.name 
+                git config --global user.email
+                set -x
+                git remote set-url origin https://github.com/santhoshvelusamy/Run-Apache.git
+                git status
+                git add .
+                git commit -m "Pushing" | true
+                git push -u origin master
+                """
+            }
+        }
     }
 
 }
